134a14bc6d50c23aadb7142631016ff2f1e504ed
Magwest1 <magnus.olstad@gmail.com>
2021-05-18 14:00:31 +0200

    Changes color

--- src/components/sliders/materialDesignSlider.js
+++ src/components/sliders/materialDesignSlider.js
@@ -1,19 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import Slider from '@material-ui/core/Slider';
-import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import '../../App.css';
 
 
 function MaterialDesignSlider(props){
-    var textField = null; 
     const [score, setScore] = useState(0);
     const [slider, setSlider] = useState(6);
     
-    const useStyles = makeStyles({
-        root: {
-        width: 300,
-        },
-    });
 
     useEffect(() => {
         console.log(props.value);
@@ -21,23 +16,42 @@ function MaterialDesignSlider(props){
         setSlider(props.value);
     }, [props.value])
 
-
-
+    const rgbcolor = "122, 108 ,250";
+    const muiTheme = createMuiTheme({
+        overrides:{
+          MuiSlider: {
+            thumb:{
+                color: "rgb("+ rgbcolor +")",
+            },
+            track: {
+                color: "rgb("+ rgbcolor +")"
+            },
+            rail: {
+              color: "rgba("+ rgbcolor +",.4)"
+            },
+            mark: {
+                color: "rgba("+ rgbcolor +",.7)"
+            }
+          }
+      }
+      });
     
     return(
         <div>
-        <Slider
-            defaultValue={slider}
-            key={`slider-${slider}`}
-            aria-labelledby="discrete-slider"
-            valueLabelDisplay="auto"
-            step={1}
-            marks
-            min={0}
-            max={12}
-            onChange={ (e, val) => setScore(val)}
-            onChangeCommitted={(e)=> props.action(score, props.id)}
-        />
+            <ThemeProvider theme={muiTheme}>
+                <Slider
+                    defaultValue={slider}
+                    key={`slider-${slider}`}
+                    aria-labelledby="discrete-slider"
+                    valueLabelDisplay="auto"
+                    step={1}
+                    marks
+                    min={0}
+                    max={12}
+                    onChange={ (e, val) => setScore(val)}
+                    onChangeCommitted={(e)=> props.action(score, props.id)}
+                />
+            </ThemeProvider>
 
         </div>
         
